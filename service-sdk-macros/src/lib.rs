029682d83ca46b48f6b2dbec5b2df8491d1e2b3e
Artemy Gordon <artemy.gordon@youhodler.com>
2026-02-04 10:07:46 +0200

    Added more convenient way to register grpc service

--- service-sdk-macros/src/lib.rs
+++ service-sdk-macros/src/lib.rs
@@ -296,6 +296,33 @@ pub fn use_signal_r_subscriber(_input: TokenStream) -> TokenStream {
     .into()
 }
 
+use quote::quote;
+use syn::{
+    parse::{Parse, ParseStream},
+    parse_macro_input,
+    Ident, Path, Result, Token, Type,
+};
+
+struct GenerateGrpcServiceArgs {
+    service_ident: Ident,
+    app_ty: Type,
+    server_path: Path,
+}
+
+impl Parse for GenerateGrpcServiceArgs {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let service_ident: Ident = input.parse()?;
+        input.parse::<Token![,]>()?;
+
+        let app_ty: Type = input.parse()?;
+        input.parse::<Token![,]>()?;
+
+        let server_path: Path = input.parse()?;
+
+        Ok(Self { service_ident, app_ty, server_path })
+    }
+}
+
 #[proc_macro]
 pub fn generate_grpc_service(input: TokenStream) -> TokenStream {
     let input: proc_macro2::TokenStream = input.into();
@@ -315,4 +342,32 @@ pub fn generate_grpc_service(input: TokenStream) -> TokenStream {
 
     }
     .into()
+}
+
+#[proc_macro]
+pub fn generate_named_grpc_service(input: TokenStream) -> TokenStream {
+    let GenerateGrpcServiceArgs { service_ident, app_ty, server_path } = parse_macro_input!(input as GenerateGrpcServiceArgs);
+
+    let expanded = quote! {
+        #[derive(Clone)]
+        pub struct #service_ident {
+            pub app_context: ::std::sync::Arc<#app_ty>,
+        }
+
+        impl #service_ident {
+            pub fn new(app_context: ::std::sync::Arc<#app_ty>) -> Self {
+                Self { app_context }
+            }
+        }
+
+        impl service_sdk::IntoGrpcServer for #service_ident {
+            type GrpcServer = #server_path<Self>;
+
+            fn into_grpc_server(self) -> Self::GrpcServer {
+                #server_path::new(self)
+            }
+        }
+    };
+
+    expanded.into()
 }
