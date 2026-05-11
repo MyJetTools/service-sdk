79136888a2cf5f0ef165b18fac53283e5b6df69a
amigin <amigin@gmail.com>
2026-05-11 17:01:31 +0300

    Refactor generate_sdk_settings_traits to simplify get_service_name method by removing suffix handling

--- service-sdk-macros/src/lib.rs
+++ service-sdk-macros/src/lib.rs
@@ -30,12 +30,7 @@ pub fn generate_sdk_settings_traits(_input: TokenStream) -> TokenStream {
     #[async_trait]
     impl service_sdk::ServiceInfo for SettingsReader {
         fn get_service_name(&self) -> &'static str {
-            if let Ok(suffix) = std::env::var("SERVICE_NAME_SUFFIX") {
-                return format!("{}-{}", env!("CARGO_PKG_NAME"), suffix).into();
-            }else{
-                env!("CARGO_PKG_NAME")
-            }
-
+            env!("CARGO_PKG_NAME")
         }
         fn get_service_version(&self) -> &'static str {
             env!("CARGO_PKG_VERSION")
